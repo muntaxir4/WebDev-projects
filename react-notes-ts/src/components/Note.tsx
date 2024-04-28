6d639041603adb4a77d4c93398adf720d7680648
muntasir <mjmullick0608@gmail.com>
2024-04-28 16:59:46 +0530

    Add BootStrap for Responsiveness and Styling

--- react-notes-ts/src/components/Note.tsx
+++ react-notes-ts/src/components/Note.tsx
@@ -12,10 +12,18 @@ function Note({ id , title, content, delNote } : newNote) {
     delNote(id);
   }
   return (
-    <div id={id} className="note">
-      <h1>{title}</h1>
-      <p>{content}</p>
-      <button onClick={handleDelete}>DELETE</button>
+    // <div id={id} className="note">
+    //   <h1>{title}</h1>
+    //   <p>{content}</p>
+    //   <button onClick={handleDelete}>DELETE</button>
+    // </div>
+    <div id={id} className="card col-sm-3 col-10 mx-2 my-2" style={{padding:"0px"}}>
+      <h1 className="card-header">{title}</h1>
+      <div className="card-body">
+        <p className="card-text">{content}</p>
+        <button className="btn btn-outline-danger" onClick={handleDelete}>DELETE</button>
+      </div>
+      
     </div>
   );
 }
