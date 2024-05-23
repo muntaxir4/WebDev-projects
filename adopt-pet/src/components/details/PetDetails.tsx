bf82f8a939297a78e6854c344d55e11a61c6465f
muntaxir4 <mjmullick0608@gmail.com>
2024-05-24 00:07:46 +0530

    Enable https

--- adopt-pet/src/components/details/PetDetails.tsx
+++ adopt-pet/src/components/details/PetDetails.tsx
@@ -20,7 +20,9 @@ export default function(){
     return (
         <div className="border-black border-2 flex flex-col gap-2 rounded-lg m-2 p-3 animate-fade-in-right backdrop-opacity-40">
             {/* {JSON.stringify(pet)} */}
-            <ImageCarousel images={pet.images}/>
+            <ImageCarousel images={pet.images.map(image=>{
+                return "https"+image.substring(4)
+            })}/>
             <div className="flex flex-col gap-1">
                 <h1 className="text-2xl text-center"><strong>{pet.name}</strong></h1>
                 <div className="flex flex-col">
